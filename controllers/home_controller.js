cbca2a9619c52150f6cc4e32a30aa08f5008b379
Sambu Santhosh <sambusanthosh@Sambus-MacBook-Air.local>
2023-11-11 13:19:48 +0530

    set up static files acces for layouys and pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,4 @@
 module.exports.home  = function(req,res){
-    return res.end('<h1>Helllo World!</h1>')
+    // return res.end('<h1>Helllo World!</h1>');
+    return res.render('home',{title: "Sambu"});
 }
