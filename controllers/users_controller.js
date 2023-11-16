0dce3cc4662c83551be831b92d22e992028dbae5
Sambu Santhosh <sambusanthosh@Sambus-MacBook-Air.local>
2023-11-16 07:33:17 +0530

    create user commit

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,105 @@
+const User = require('../models/user');
+
 module.exports.profile = function(req,res){
     return res.render('user_profile',{title: "Profile"});
+}
+
+module.exports.signin = function(req,res){
+    return res.render('user_sign_in',{title: "Codeial | Signin"});
+}
+
+module.exports.signup = function(req,res){
+    return res.render('user_sign_up',{title: "Codeial | Signup"});
+}
+
+
+
+module.exports.create = function(req,res){
+
+    if (req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    // const resu = User.findOne({email : req.body.mail});
+    // console.log(!resu==false);
+
+    User.findOne({email : req.body.mail}).then((user)=>
+    
+    {   
+        console.log(user);
+        if (!user){
+            User.create(req.body).then((user)=>{
+                return res.redirect('/users/sign-in'); 
+            })
+            .catch(err => {
+                return res.redirect('back');
+            })
+            
+        }
+    }
+
+    )
+    .catch(err => {console.log('error in finding user')})
+
+
+    // if (resu){
+
+    //     debugger;
+
+    //     console.log('apple');
+
+    //     try {
+    //         console.log('ball');
+    //         User.create(req.body);
+    //         return res.redirect('/users/sign-in');
+    //     }
+
+    //     catch (e){
+    //         console.log(e);
+    //         return
+    //     }
+        
+    // }
+
+    // else{
+    //     return res.redirect('/users/sign-in');
+    // }
+}
+//     
+    
+  
+
+    
+
+    // User.findOne({email : req.body.mail},function(err,user){
+
+    //     if (err){
+    //         console.log('error in finding in user while signing up');
+    //         return
+    //     }
+
+    //     if (!user){
+
+            
+    //         User.create(req.body,function(err,user){
+
+    //             if (err){
+    //                 console.log('error while creating the user');
+    //                 return
+    //             }
+    //             return res.redirect('/users/sign-in');
+
+    //         });
+    //     }
+
+    //     else{
+    //         return res.redirect('back');
+    //     }
+
+    // });
+
+// }
+
+module.exports.createsession = function(req,res){
+
 }
--- routes/users.js
+++ routes/users.js
@@ -4,4 +4,10 @@ const usersController = require('../controllers/users_controller');
 
 router.get('/profile',usersController.profile);
 
+router.get('/sign-in',usersController.signin);
+
+router.get('/sign-up',usersController.signup);
+
+router.post('/create',usersController.create);
+
 module.exports = router;
